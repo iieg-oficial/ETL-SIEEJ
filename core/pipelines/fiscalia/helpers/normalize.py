2b585c9a990b5f24eb3ebefb6f10bf7d56c43511
velazco-joseh <jose.velazco@iieg.gob.mx>
2026-02-05 10:19:06 -0600

    feat(fiscalia): adds normalize helpers module

---
+++ core/pipelines/fiscalia/helpers/normalize.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import unicodedata
+
+def lowercase_headers(df: pd.DataFrame)-> pd.DataFrame:
+    df.columns = map(str.lower, df.columns)
+
+def lowercase_df(df: pd.DataFrame)-> pd.DataFrame:
+    string_cols = df.select_dtypes(include=['object', 'string']).columns
+    df[string_cols] = df[string_cols].apply(lambda x: x.str.lower())
+    return df
+
+def lowercase_col(df: pd.DataFrame, col:str) -> None:
+    df[col] = df[col].str.lower().copy()
+
+def uppercase_col(df: pd.DataFrame, col:str) -> None:
+    df[col] = df[col].str.upper().copy()
+
+def title_col(df: pd.DataFrame, col:str) -> None:
+    df[col] = df[col].str.title().copy()
+
+def normalize_col(df: pd.DataFrame, col: str) -> pd.Series:
+    normalized = lowercase_col(df, col)
+    normalized = df[col].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+    normalized = normalized.str.replace(' ', '_')
+    return normalized
+
+def normalize_text(text):
+   text  = text.replace(' ', '_')
+   return ''.join(c for c in unicodedata.normalize('NFD', text)
+                  if unicodedata.category(c) != 'Mn')
+
+def list_values_to_null(df: pd.DataFrame, rm_list: dict = None) -> pd.DataFrame:
+    df_copy = df.copy()
+    for col in df_copy.select_dtypes(include='str').columns:
+        df_copy[col] = df_copy[col].replace(
+            {val: None for pattern in rm_list for val in [pattern, pattern.lower(), pattern.upper()]}
+        )
+    return df_copy
+
+
