6e1effb489652ff6e4dd2480a658aedcc81a34ad
velazco-joseh <jose.velazco@iieg.gob.mx>
2026-02-06 09:56:51 -0600

    update(fiscalia): adds drops duplicates and nans to nulls functionalities

--- core/pipelines/fiscalia/helpers/normalize.py
+++ core/pipelines/fiscalia/helpers/normalize.py
@@ -9,26 +9,6 @@ def lowercase_df(df: pd.DataFrame)-> pd.DataFrame:
     df[string_cols] = df[string_cols].apply(lambda x: x.str.lower())
     return df
 
-def lowercase_col(df: pd.DataFrame, col:str) -> None:
-    df[col] = df[col].str.lower().copy()
-
-def uppercase_col(df: pd.DataFrame, col:str) -> None:
-    df[col] = df[col].str.upper().copy()
-
-def title_col(df: pd.DataFrame, col:str) -> None:
-    df[col] = df[col].str.title().copy()
-
-def normalize_col(df: pd.DataFrame, col: str) -> pd.Series:
-    normalized = lowercase_col(df, col)
-    normalized = df[col].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
-    normalized = normalized.str.replace(' ', '_')
-    return normalized
-
-def normalize_text(text):
-   text  = text.replace(' ', '_')
-   return ''.join(c for c in unicodedata.normalize('NFD', text)
-                  if unicodedata.category(c) != 'Mn')
-
 def list_values_to_null(df: pd.DataFrame, rm_list: dict = None) -> pd.DataFrame:
     df_copy = df.copy()
     for col in df_copy.select_dtypes(include='str').columns:
@@ -37,4 +17,28 @@ def list_values_to_null(df: pd.DataFrame, rm_list: dict = None) -> pd.DataFrame:
         )
     return df_copy
 
+def lowercase_col(df: pd.DataFrame, col:str) -> None:
+    df[col] = df[col].str.lower()
+
+def uppercase_col(df: pd.DataFrame, col:str) -> None:
+    df[col] = df[col].str.upper()
+
+def title_col(df: pd.DataFrame, col:str) -> None:
+    df[col] = df[col].str.title()
+
+def normalize_col(df: pd.DataFrame, col: str) -> pd.Series:
+    normalized = df[col].str.lower()
+    normalized = normalized.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+    normalized = normalized.str.replace(' ', '_')
+    return normalized
+
+def drop_duplicates_col(df: pd.DataFrame,column: str) -> pd.DataFrame:
+    normalized = normalize_col(df, column)
+    mask = ~normalized.duplicated(keep='first')
+    return df[mask]
+
+def normalize_text(text):
+   text  = text.replace(' ', '_').lower()
+   return ''.join(c for c in unicodedata.normalize('NFD', text)
+                  if unicodedata.category(c) != 'Mn')
 
