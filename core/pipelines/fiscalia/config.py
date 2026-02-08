471b3c1aab03bd35ceb9c7243bd3ee9e720eda37
velazco-joseh <jose.velazco@iieg.gob.mx>
2026-02-08 16:00:39 -0600

    update(fiscalia): config to use base configuration

--- core/pipelines/fiscalia/config.py
+++ core/pipelines/fiscalia/config.py
@@ -1,23 +1,10 @@
-import json
-from typing import Literal, Optional
-from pydantic import Field
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import SettingsConfigDict
+from core.config import BaseConfig, env_path
 
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file="core/pipelines/fiscalia/.env")
-    LOG_LEVEL: str = "INFO"
-    DB_USER: Optional[str] = Field(default="Test")
-    DB_PASSWORD: Optional[str] = Field(default="Test")
-    DB_HOST: Optional[str] = Field(default="Test")
-    DB_PORT: Optional[str] = Field(default="Test")
-    DB_NAME: Optional[str] = Field(default="Test")
+class Settings(BaseConfig):
+    model_config = SettingsConfigDict(env_file=env_path("fiscalia"))
 
-    G_FOLDER_URL: str
-
-
-
-    @property
-    def get_database_url(self) -> str:
-        return f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    BOOTSTRAP_CSV_FOLDER: str
+    UPDATE_EXCEL_FOLDER: str
 
 settings = Settings()
