bcbebdfac2255baa2b142f5c27a05f054dfc4e76
velazco-joseh <jose.velazco@iieg.gob.mx>
2026-02-11 12:38:18 -0600

    feat(core): implements clean and normalize modules

---
+++ core/utils/normalize.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import unicodedata
+
+def lowercase_headers(df: pd.DataFrame)-> pd.DataFrame:
+    df.columns = map(str.lower, df.columns)
+
+def lowercase_df(df: pd.DataFrame)-> pd.DataFrame:
+    string_cols = df.select_dtypes(include=['object', 'string']).columns
+    df[string_cols] = df[string_cols].apply(lambda x: x.str.lower())
+    return df
+
+def titlecase_df(df: pd.DataFrame)-> pd.DataFrame:
+    string_cols = df.select_dtypes(include=['object', 'string']).columns
+    df[string_cols] = df[string_cols].apply(lambda x: x.str.title())
+    return df
+
+def lowercase_col(df: pd.DataFrame, col:str) -> None:
+    df[col] = df[col].str.lower()
+
+def uppercase_col(df: pd.DataFrame, col:str) -> None:
+    df[col] = df[col].str.upper()
+
+def title_col(df: pd.DataFrame, col:str) -> None:
+    df[col] = df[col].str.title()
+
+def normalize_col(df: pd.DataFrame, col: str) -> pd.Series:
+    normalized = df[col].str.lower()
+    normalized = normalized.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+    normalized = normalized.str.replace(' ', '_')
+    return normalized
+
+def normalize_text(text):
+   text  = text.replace(' ', '_').lower()
+   return ''.join(c for c in unicodedata.normalize('NFD', text)
+                  if unicodedata.category(c) != 'Mn')
