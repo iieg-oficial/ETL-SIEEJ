d9c35e183269fbae8d64896dae836221baf9b0d1
velazco-joseh <jose.velazco@iieg.gob.mx>
2026-02-24 09:32:57 -0600

    fix(fiscalia): migration view sql

--- migrations/fiscalia/sql/V4__vista_fiscalia.sql
+++ migrations/fiscalia/sql/V4__vista_fiscalia.sql
@@ -16,12 +16,20 @@ SELECT
     c.fecha_denuncia,
     c.fecha_actualizacion
 FROM casos c
-LEFT JOIN delitos d ON c.delitos_id = d.id
-LEFT JOIN bien_afectado ba ON d.bien_afectado_id = ba.id
-LEFT JOIN violencia v ON c.violencia_id = v.id
-LEFT JOIN zonas_geograficas zg ON c.zonas_geograficas_id = zg.id
-LEFT JOIN cvegeo_municipalities m ON c.municipios_id = m.id
-LEFT JOIN colonias col ON c.colonias_id = col.id
-LEFT JOIN calles cal ON c.calles_id = cal.id
-LEFT JOIN cruces cr ON c.cruces_id = cr.id;
-
+LEFT JOIN delitos d
+    ON c.delitos_id = d.id
+LEFT JOIN bien_afectado ba
+    ON d.bien_afectado_id = ba.id
+LEFT JOIN violencia v
+    ON c.violencia_id = v.id
+LEFT JOIN zonas_geograficas zg
+    ON c.zonas_geograficas_id = zg.id
+LEFT JOIN cvegeo_municipalities m
+    ON c.municipios_id = m.cve_mun
+    AND m.cve_ent = 14
+LEFT JOIN colonias col
+    ON c.colonias_id = col.id
+LEFT JOIN calles cal
+    ON c.calles_id = cal.id
+LEFT JOIN cruces cr
+    ON c.cruces_id = cr.id;
