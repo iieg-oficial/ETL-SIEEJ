c533fd7438e3a5826dc1e8444d7489db2e843e74
velazco-joseh <jose.velazco@iieg.gob.mx>
2026-03-10 11:06:26 -0600

    feat(centros-educativos): adds apply accents helper functionality

---
+++ core/pipelines/centros_educativos/helpers/accents.py
@@ -0,0 +1,12 @@
+import re
+from core.pipelines.centros_educativos.mappings.accent_mappings import ACCENT_MAP
+
+def apply_accents(value: str) -> str:
+    if not isinstance(value, str):
+        return value
+    upper_val = value.upper()
+    for pattern, replacement in ACCENT_MAP.items():
+        upper_val = re.sub(pattern, replacement, upper_val)
+    if value.istitle():
+        return upper_val.title()
+    return upper_val.capitalize()
