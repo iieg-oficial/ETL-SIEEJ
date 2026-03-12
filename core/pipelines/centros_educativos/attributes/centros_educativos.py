b9e319da54e4ff1e10c94dade704f20399e94e03
velazco-joseh <jose.velazco@iieg.gob.mx>
2026-03-12 11:41:49 -0600

    feat(centros-educativos): implements attributes for database tables

---
+++ core/pipelines/centros_educativos/attributes/centros_educativos.py
@@ -0,0 +1,13 @@
+from enum import auto, StrEnum
+
+class CentrosEducativosTables(StrEnum):
+    TURNOS = auto()
+    TIPOS_EDUCATIVOS = auto()
+    NIVELES_EDUCATIVOS = auto()
+    SERVICIOS_EDUCATIVOS = auto()
+    TIPOS_CONTROLES = auto()
+    TIPOS_SOSTENIMIENTO = auto()
+    LOCALIDADES = auto()
+    DOMICILIOS = auto()
+    COLONIAS = auto()
+    CENTROS = auto()
