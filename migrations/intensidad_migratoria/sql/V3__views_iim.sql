7d1ca5dd897e911cbf9fc47d31821796f0b09737
velazco-joseh <jose.velazco@iieg.gob.mx>
2026-03-27 13:27:29 -0600

    feat(intensidad-migratoria): adds database migrations

---
+++ migrations/intensidad_migratoria/sql/V3__views_iim.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE VIEW view_iim_entidades AS
+SELECT
+    e.entidad_id,
+    s.nom_ent               AS entidad,
+    e.viv_totales,
+    e.por_viv_remesas,
+    e.por_viv_emigrantes,
+    e.por_viv_reto,
+    e.iim_dp2,
+    RANK() OVER (PARTITION BY e.fecha ORDER BY e.iim_dp2 ASC) AS lugar,
+    e.fecha
+FROM iim_estatal e
+LEFT JOIN cvegeo_states s ON s.cve_ent = e.entidad_id;
+
+CREATE OR REPLACE VIEW view_iim_municipios_jalisco AS
+SELECT
+    i.municipio_id,
+    m.nomgeo                AS municipio,
+    i.viv_totales,
+    i.por_viv_remesas,
+    i.por_viv_emigrantes,
+    i.por_viv_reto,
+    i.iim_dp2,
+    i.fecha
+FROM iim_municipal i
+LEFT JOIN cvegeo_municipalities m ON m.cvegeo = i.municipio_id
+WHERE m.cve_ent = 14;
