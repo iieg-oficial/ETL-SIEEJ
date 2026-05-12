5eec05434f6443b416ecea89b39d045cda4318b0
Hector Moreno <h.morenocarrion@gmail.com>
2026-05-12 14:58:36 -0600

    refactor(delitos_fuero_comun): move load and transform back to stages folder

--- dags/etl_delitos_fuero_comun.py
+++ dags/etl_delitos_fuero_comun.py
@@ -15,8 +15,8 @@ except ImportError:
 
 from core.pipeline import Pipeline
 from core.pipelines.delitos_fuero_comun.stages.extract import DelitosExtract
-from core.constants.load import DelitosLoad
-from core.constants.transform import DelitosTransform
+from core.pipelines.delitos_fuero_comun.stages.load import DelitosLoad
+from core.pipelines.delitos_fuero_comun.stages.transform import DelitosTransform
 
 
 def run_bootstrap() -> None:
