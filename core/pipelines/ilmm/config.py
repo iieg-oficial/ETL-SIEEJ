9010720a43146a6ca8e6c63bfe8dc13bb2249ddc
Hector Moreno <hector.moreno@iieg.gob.mx>
2026-06-01 12:26:05 -0600

    chore(config): apply ruff-format to ilmm config

--- core/pipelines/ilmm/config.py
+++ core/pipelines/ilmm/config.py
@@ -10,9 +10,7 @@ class Settings(BaseConfig):
     ILMM_BASE_URL: str = Field(
         default="https://www.inegi.org.mx/contenidos/programas/ilmm/datosabiertos/conjunto_de_datos_ilmm_{year}_1t_csv.zip"
     )
-    CSV_INNER_PATH: str = Field(
-        default="conjunto_de_datos/conjunto_de_datos_ilmm_{year}_1t.csv"
-    )
+    CSV_INNER_PATH: str = Field(default="conjunto_de_datos/conjunto_de_datos_ilmm_{year}_1t.csv")
     BOOTSTRAP_YEARS: list[int] = Field(default=[2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024])
 
     @field_validator("BOOTSTRAP_YEARS", mode="before")
