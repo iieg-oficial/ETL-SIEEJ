669f542c28fcb2e8bad8136649e8558c728e1474
alejandroiieg <alejandro.zarate@iieg.gob.mx>
2026-07-20 13:23:04 -0600

    feat(efipem): add materialized views refresh and queries module

---
+++ core/pipelines/efipem/queries/views.py
@@ -0,0 +1,13 @@
+MATERIALIZED_VIEWS = [
+    "ingresos_totales",
+    "ingresos_totales_reales_precios_2023",
+    "ingresos_totales_reales_per_capita_precios_2023",
+    "ingresos_participaciones",
+    "ingresos_financiamiento",
+    "porcentaje_ingresos_participaciones",
+    "porcentaje_ingresos_financiamiento",
+    "porcentaje_ingresos_propios",
+    "egresos_totales",
+    "egresos_deuda_publica",
+    "porcentaje_egresos_deuda_publica",
+]
