57bccf3dea070f83501bea3203f8a120ab11e03d
Hector Moreno <hector.moreno@iieg.gob.mx>
2026-08-26 15:00:53 -0600

    refactor(migrations): name fdw placeholders after their database

---
+++ migrations/delitos_fuero_comun/sql/V15__fdw_placeholders_por_nombre_db.sql
@@ -0,0 +1,14 @@
+-- =======================================================================
+-- V15: homologar cvegeo_server a placeholders FDW nombrados por la DB real
+-- =======================================================================
+
+ALTER SERVER cvegeo_server OPTIONS (
+    SET dbname '${fdw_cvegeo_dbname}',
+    SET host   '${fdw_cvegeo_host}',
+    SET port   '${fdw_cvegeo_port}'
+);
+
+ALTER USER MAPPING FOR CURRENT_USER SERVER cvegeo_server OPTIONS (
+    SET user     '${fdw_cvegeo_user}',
+    SET password '${fdw_cvegeo_password}'
+);
