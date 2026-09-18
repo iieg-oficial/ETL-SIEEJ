be65a5b3cbdc4b1e691a5fbf52507be76b924d5a
Hector Moreno <hector.moreno@iieg.gob.mx>
2026-09-18 12:59:41 -0600

    refactor(delitos_fuero_comun): declarar MATERIALIZED_VIEWS y migrar refresco al helper compartido

--- core/pipelines/delitos_fuero_comun/queries/__init__.py
+++ core/pipelines/delitos_fuero_comun/queries/__init__.py
@@ -1,3 +1,3 @@
-from core.pipelines.delitos_fuero_comun.queries.views import REFRESH_GOLD, REFRESH_SECRETARIADO
+from core.pipelines.delitos_fuero_comun.queries.views import MATERIALIZED_VIEWS, REFRESH_GOLD
 
-__all__ = ["REFRESH_GOLD", "REFRESH_SECRETARIADO"]
+__all__ = ["MATERIALIZED_VIEWS", "REFRESH_GOLD"]
